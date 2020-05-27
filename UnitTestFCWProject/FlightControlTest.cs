d4dfce3b450e0ce089a4d49e7a6cbc48ce48e021
YaelSim <58948260+YaelSim@users.noreply.github.com>
2020-05-27 15:56:24 +0300

    open unit test

---
+++ UnitTestFCWProject/FlightControlTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlightControlWeb;
+using FlightControlWeb.Controllers;
+using FlightControlWeb.Models;
+
+
+namespace UnitTestFCWProject
+{
+    [TestClass]
+    public class FlightControlTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
